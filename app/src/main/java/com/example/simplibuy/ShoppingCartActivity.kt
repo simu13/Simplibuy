7d64508693a23fba3aa79905ccb2a5686244490f
akshitverma4 <akshitverma824@gmail.com>
2020-10-08 21:36:36 +0530

    First Commit

---
+++ app/src/main/java/com/example/simplibuy/ShoppingCartActivity.kt
@@ -0,0 +1,11 @@
+package com.example.simplibuy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ShoppingCartActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shopping_cart)
+    }
+}
